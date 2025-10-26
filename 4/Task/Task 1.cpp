bf62249d601b9b9c7eb478dc76b61a8693ed7bf6
NRM06 <nathan.maglasang06@gmail.com>
2025-10-26 20:44:50 +1100

    Finished Week 4

--- 4/Task/Task 1.cpp
+++ 4/Task/Task 1.cpp
@@ -122,28 +122,75 @@ void factor_Task4(lint n) {
 
 int main() {
 int selection = 0;
-	cout << "Select which tasks you want to complete:\n1. Task 1\n2. Task 2,3,4\nSelection";
+	lint n,k;
+
+	cout << "Select which tasks you want to complete:\n1. Task 1\n2. Task 2,3,4\nSelection: ";
+	cin >> selection;
 if (selection != 1 && selection != 2) {
-	cout << "Ensure you select either 1 or 2: \n1. Task 1\n2. Task 2,3,4\nSelection";
+	cout << "Ensure you select either 1 or 2: \n1. Task 1\n2. Task 2,3,4\nSelection: ";
 }
-lint n,k;
-cout << "Task 1\nInput a pair of integers (n, k)\nInput n: ";
-cin >> n;
-lint original_n = n;
-cout << "Input k: ";
-cin >> k;
-cout << "\n--- Task 1: Compute Mk ---\n";
-lint Mk = compute_Task_1(n, k);
-cout << "Mk = " << Mk << "\n";
+	switch (selection) {
+		case 1: {
+			cout << "Task 1\nInput a pair of integers (n, k)\nInput n: ";
+			cin >> n;
+			cout << "Input k: ";
+			cin >> k;
+			cout << "\n--- Task 1: Compute Mk ---\n";
+			lint Mk = compute_Task_1(n, k);
+			cout << "Mk = " << Mk << "\n";
+		}
+			break;
 
-cout << "\n--- Task 2: Find nontrivial factor ---\n";
-lint factor = Factor_Task_2(n);
-cout << "Factor = " << factor << "\n";
+		case 2: {
+			cout << "Task 2, 3, 4\nInput a pair of integers (n, k)\nInput n: ";
+			cin >> n;
+			cout << "Input k: ";
+			cin >> k;
+			lint original_n = n;
+			cout << "\n--- Task 2: Find nontrivial factor ---\n";
+			lint factor = Factor_Task_2(n);
+			cout << "Factor = " << factor << "\n";
 
-cout << "\n--- Task 3: Find prime factor ---\n";
-lint prime = findPrimeFactor_Task3(n);
-cout << "Prime factor = " << prime << "\n";
+			cout << "\n--- Task 3: Find prime factor ---\n";
+			lint prime = findPrimeFactor_Task3(n);
+			cout << "Prime factor = " << prime << "\n";
 
-cout << "\n--- Task 4: Full factorization ---\n";
-factor_Task4(original_n);
+			cout << "\n--- Task 4: Full factorization ---\n";
+			factor_Task4(original_n);
+		}
+			break;
+
+
+	}
+
+
+
 }
+
+
+//TASK 4 OUTPUTS:
+/*
+Factoring 359446091074757:
+673 127 401 131 223 359
+Factoring 15419616932490265564907:
+(FAILS AS THE LONG LONG INT MAX SIZE IS 9.22 × 10¹⁸ but the given value is 15,419,616,932,490,265,564,907 ≈ 1.5 × 10²²)
+This can be fixed but I need to use the boost library.
+
+ */
+//Questions
+/*
+(1) Due to the methods of calculation, increases to the value of k! increase exponentially, where when k is 5, k! is 120, whereas when k is 10, k! is 3.6 Million
+In turn this massively increases the time taken for the the calculations to complete
+
+Additionally, in task 1, with the way I setup my computations, for each value of k, the calculations for k! are recalculated from the beggining for each iteration,
+rather than using the previous value of k!, taking up large amounts of memory as the value for k gets larger (it would, once i figure out a simpler way to get around the long long int limits)
+
+Similarly for the value of Mk, when calculating the value for Mk + 1, the value for Mk is recalculated, where it could have been stored.
+
+(2) One way to make these algorithms much faster would be to use the Miller-Rabin method for primality testing, as using the method above checks every value between i and sqrt(n) to discern their primality
+Using the Miller Rabin method would use modular arithmetic to calculate primality for much larger numbers such as 64 bits with far less iterations.
+
+
+
+
+ */
